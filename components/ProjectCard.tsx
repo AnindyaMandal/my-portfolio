dba0ae9ce61eadfecb5d64cb50c5eb8a0284341d
AnindyaMandal <anindya.cpsc@gmail.com>
2023-09-08 21:48:56 -0400

    Added best projects to ProjectCard

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,18 +1,52 @@
+"use client"
+
 import "../styles/globals.css"
 import Link from 'next/link'
 import { ProjectInfo } from "@/utils/ProjectInfo"
+import { useState } from "react"
+
+
+
 
 const ProjectCard = (info : ProjectInfo) => {
+
+  const [toggleExpandedCardView, setToggleExpandedCardView] = useState(false)
+
+  const setToggleCSS = () => {
+    if (toggleExpandedCardView) {
+      return 'black_btn_card transition_time'
+    }
+
+    return 'black_btn_prj transition_time'
+
+  }
+
+
+
   return (
     
-        <Link 
-            href="\"
-            className='black_btn_prj'
+        <div 
+            className={setToggleCSS()}
+            onMouseEnter={() => setToggleExpandedCardView((prev) => (!prev))}
+            onMouseLeave={() => setToggleExpandedCardView((prev) => (!prev))}
             >
-            {info.name}
-            <br />
-            {info.vid_url}
-        </Link>
+            <div>
+            <h1>{info.name}</h1>
+            {toggleExpandedCardView && 
+            <>
+            <br></br>
+            <p>{info.description}</p>
+            </>
+            }
+            </div>
+            {toggleExpandedCardView && 
+            <>
+            <iframe className="transition_time_long" src={info.vid_url} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
+            
+            </>
+            }
+            
+        </div>
   )
 }
 
