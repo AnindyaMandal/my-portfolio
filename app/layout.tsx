e8dd8fd334da096faac538a69c6cc28a7ff99026
AnindyaMandal <anindya.cpsc@gmail.com>
2023-10-30 17:19:03 -0400

    Removed Resume from Homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,21 @@
-import '../styles/globals.css'
-import Nav from "@/components/Nav"
+import "../styles/globals.css";
+import Nav from "@/components/Nav";
 
 export const metadata = {
-    title: "Portfolio Site",
-    description: "Andy's Portfolio Site"
-}
+	title: "Portfolio Site",
+	description: "Andy's Portfolio Site",
+};
 
-const layout = ({children} : any) => {
-  return (
-    <html lang="en">
-        <body>
-            <Nav />
-            <main className="main">
-                {children}
-            </main>
-            {/* {children} */}
-        </body>
-    </html>
-  )
-}
+const layout = ({ children }: any) => {
+	return (
+		<html lang="en">
+			<body>
+				<Nav />
+				<main className="main">{children}</main>
+				{/* {children} */}
+			</body>
+		</html>
+	);
+};
 
-export default layout
+export default layout;
--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,13 @@ import "../styles/globals.css";
 const page = () => {
 	return (
 		<section>
-			<div className="m-10">
+			{/* <div className="m-10">
 				<embed
 					src="/images/my_resume.pdf"
 					type="application/pdf"
 					className="pdf_view"
 				></embed>
-			</div>
+			</div> */}
 		</section>
 	);
 };
